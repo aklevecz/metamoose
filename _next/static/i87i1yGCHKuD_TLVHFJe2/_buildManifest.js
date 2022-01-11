d4b8ba2f130ae78d825b8ae3776fb9732fff659e
aklevecz <teh@raptor.pizza>
2022-01-11 11:51:50 -0800

    Updates

---
+++ _next/static/i87i1yGCHKuD_TLVHFJe2/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/11d837d1-ae7046c52f420a82.js","static/css/63599ec53b6906c8.css","static/chunks/pages/index-dc09cdbb64a95059.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
